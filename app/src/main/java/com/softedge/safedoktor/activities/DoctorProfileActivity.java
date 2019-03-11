350dc6e5440edcd653ad655e932b45696613d8e4
Jedi_Supreme <jediassisi@gmail.com>
2019-03-11 15:46:16 +0000

    completed doc profile, working on review

---
+++ app/src/main/java/com/softedge/safedoktor/activities/DoctorProfileActivity.java
@@ -0,0 +1,33 @@
+package com.softedge.safedoktor.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.softedge.safedoktor.R;
+
+public class DoctorProfileActivity extends AppCompatActivity {
+
+    //========================================ON CREATE=============================================
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_profile);
+
+    }
+    //========================================ON CREATE=============================================
+
+    //--------------------------------------------BUTTON CLICK LISTENERS----------------------------
+    public void ReviewDoc(View view) {
+    }
+
+    public void VideoChatDoc(View view) {
+    }
+
+    public void TextChatDoc(View view) {
+    }
+
+    public void VoiceCallDoc(View view) {
+    }
+    //--------------------------------------------BUTTON CLICK LISTENERS----------------------------
+}
---
+++ app/src/main/java/com/softedge/safedoktor/activities/ReviewActivity.java
@@ -0,0 +1,15 @@
+package com.softedge.safedoktor.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.softedge.safedoktor.R;
+
+public class ReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+    }
+}
